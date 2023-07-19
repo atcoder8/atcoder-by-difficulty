ac9e56c7f17a5b8a76283a98e8edf840d6c4d896
atcoder8 <atcoder8@gmail.com>
2023-07-19 17:49:54 +0900

    Solved ABC201-D again.

--- src/bin/abc-d/abc201-d.rs
+++ src/bin/abc-d/abc201-d.rs
@@ -1,57 +1,57 @@
+use std::cmp::Ordering;
+
 use proconio::{input, marker::Chars};
 
 fn main() {
+    println!(
+        "{}",
+        match solve() {
+            Ordering::Greater => "Takahashi",
+            Ordering::Less => "Aoki",
+            Ordering::Equal => "Draw",
+        }
+    );
+}
+
+fn solve() -> Ordering {
     input! {
         (h, w): (usize, usize),
         aaa: [Chars; h],
     }
 
-    let mut effect = vec![vec![0; w]; h];
-    for i in 0..h {
-        for j in 0..w {
-            effect[i][j] = if (aaa[i][j] == '+') == ((i + j) % 2 == 1) {
-                1
-            } else {
-                -1
-            };
+    let effect = |i: usize, j: usize| {
+        if i == 0 && j == 0 {
+            0
+        } else if (aaa[i][j] == '+') ^ ((i + j) % 2 == 0) {
+            1
+        } else {
+            -1
         }
-    }
-    effect[0][0] = 0;
+    };
 
     let mut dp = vec![vec![0; w]; h];
-    dp[h - 1][w - 1] = effect[h - 1][w - 1];
-
+    dp[h - 1][w - 1] = effect(h - 1, w - 1);
     for i in (0..h).rev() {
         for j in (0..w).rev() {
-            if (i, j) == (h - 1, w - 1) {
+            if i == h - 1 && j == w - 1 {
                 continue;
             }
 
-            let mut candidate = vec![];
+            let mut candidates = vec![];
             if i < h - 1 {
-                candidate.push(dp[i + 1][j]);
+                candidates.push(dp[i + 1][j]);
             }
             if j < w - 1 {
-                candidate.push(dp[i][j + 1]);
+                candidates.push(dp[i][j + 1]);
             }
 
-            let mut best = if (i + j) % 2 == 0 {
-                candidate.into_iter().max().unwrap()
+            dp[i][j] = if (i + j) % 2 == 0 {
+                candidates.iter().max().unwrap()
             } else {
-                candidate.into_iter().min().unwrap()
-            };
-
-            best += effect[i][j];
-
-            dp[i][j] = best;
+                candidates.iter().min().unwrap()
+            } + effect(i, j);
         }
     }
 
-    if dp[0][0] > 0 {
-        println!("Takahashi");
-    } else if dp[0][0] < 0 {
-        println!("Aoki");
-    } else {
-        println!("Draw");
-    }
+    dp[0][0].cmp(&0)
 }
