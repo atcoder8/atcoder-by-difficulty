0498882cc4b272cd663e7dff053fa56a0d263fed
atcoder8 <atcoder8@gmail.com>
2023-10-07 12:49:21 +0900

    Solved ABC266-G.

---
+++ src/bin/abc-g/abc266-g.rs
@@ -0,0 +1,627 @@
+use factorial::Factorial;
+use modint2::Modint998244353;
+use proconio::input;
+
+type Mint = Modint998244353;
+
+fn main() {
+    input! {
+        r: usize,
+        g: usize,
+        b: usize,
+        k: usize,
+    }
+
+    println!("{}", solve(r, g, b, k));
+}
+
+fn solve(r: usize, g: usize, b: usize, k: usize) -> Mint {
+    let mut fac = Factorial::<Mint>::new();
+
+    fac.combinations(b + g, b)
+        * fac.combinations(g, k)
+        * fac.combinations_with_repetition(b + k + 1, r - k)
+}
+
+/// Module for testing
+#[cfg(test)]
+mod random_test {
+    use itertools::Itertools;
+    use rand::Rng;
+
+    use crate::Mint;
+
+    /// Input data type.
+    type Input = (usize, usize, usize, usize);
+
+    /// Output data type.
+    type Output = Mint;
+
+    /// Perform the specified number of tests.
+    #[test]
+    fn test() {
+        const NUMBER_OF_TESTS: usize = 1000;
+
+        for test_case_index in 0..NUMBER_OF_TESTS {
+            let input = generator();
+            let expected_output = expected(input.clone());
+            let actual_output = solve(input.clone());
+
+            assert_eq!(
+                expected_output, actual_output,
+                "
+Wrong Answer on Test #{}
+
+[Input]
+{:?}
+
+[Expected output]
+{:?}
+
+[Actual output]
+{:?}
+",
+                test_case_index, input, expected_output, actual_output
+            );
+        }
+    }
+
+    /// Generate test cases.
+    pub fn generator() -> Input {
+        let mut rng = rand::thread_rng();
+
+        let mut rem = rng.gen_range(3..=12);
+
+        let r = rng.gen_range(1..=(rem - 2));
+        rem -= r;
+
+        let g = rng.gen_range(1..=(rem - 1));
+        rem -= g;
+
+        let b = rem;
+
+        let k = rng.gen_range(0..=r.min(g));
+
+        (r, g, b, k)
+    }
+
+    /// Return the expected answer.
+    pub fn expected(input: Input) -> Output {
+        let (r, g, b, k) = input;
+
+        let mut cnt = Mint::new(0);
+
+        let mut stack: Vec<Vec<char>> = vec![vec![]];
+        while let Some(cur) = stack.pop() {
+            let (mut r_cnt, mut g_cnt, mut b_cnt) = (0, 0, 0);
+            for &c in &cur {
+                match c {
+                    'R' => r_cnt += 1,
+                    'G' => g_cnt += 1,
+                    'B' => b_cnt += 1,
+                    _ => panic!(),
+                }
+            }
+
+            if r_cnt == r
+                && g_cnt == g
+                && b_cnt == b
+                && cur.windows(2).positions(|win| win == ['R', 'G']).count() == k
+            {
+                cnt += 1;
+                continue;
+            }
+
+            if r_cnt < r {
+                let mut next = cur.clone();
+                next.push('R');
+                stack.push(next);
+            }
+
+            if g_cnt < g {
+                let mut next = cur.clone();
+                next.push('G');
+                stack.push(next);
+            }
+
+            if b_cnt < b {
+                let mut next = cur.clone();
+                next.push('B');
+                stack.push(next);
+            }
+        }
+
+        cnt
+    }
+
+    /// Test this program.
+    pub fn solve(input: Input) -> Output {
+        let (r, g, b, k) = input;
+
+        super::solve(r, g, b, k)
+    }
+}
+
+pub mod modint2 {
+    //! This module implements modular arithmetic.
+
+    use std::{
+        iter::{Product, Sum},
+        ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign},
+    };
+
+    type InnerType = u32;
+
+    /// Returns `x` such that `a * x` is equivalent to `1` with `m` as the modulus.
+    fn modinv(a: u32, m: u32) -> u32 {
+        let (mut a, mut b, mut s, mut t) = (a as i64, m as i64, 1, 0);
+        while b != 0 {
+            let q = a / b;
+            a -= q * b;
+            std::mem::swap(&mut a, &mut b);
+            s -= q * t;
+            std::mem::swap(&mut s, &mut t);
+        }
+
+        assert_eq!(
+            a.abs(),
+            1,
+            "\
+    There is no multiplicative inverse of `a` with `m` as the modulus, \
+    because `a` and `m` are not prime to each other (gcd(a, m) = {}).",
+            a.abs()
+        );
+
+        ((s % m as i64 + m as i64) % m as i64) as u32
+    }
+
+    pub trait Reminder {
+        /// Returns the remainder divided by `modulus`.
+        fn reminder(self, modulus: InnerType) -> InnerType;
+    }
+
+    macro_rules! impl_reminder_for_small_unsigned_int {
+        ($($unsigned_small_int: tt), *) => {
+            $(
+                impl Reminder for $unsigned_small_int {
+                    fn reminder(self, modulus: InnerType) -> InnerType {
+                        self as InnerType % modulus
+                    }
+                }
+            )*
+        };
+    }
+
+    // Implements `Reminder` trait for `u8`, `u16` and `u32`.
+    impl_reminder_for_small_unsigned_int!(u8, u16, u32);
+
+    macro_rules! impl_reminder_for_large_unsigned_int {
+        ($($unsigned_large_int: tt), *) => {
+            $(
+                impl Reminder for $unsigned_large_int {
+                    fn reminder(self, modulus: InnerType) -> InnerType {
+                        (self % modulus as Self) as InnerType
+                    }
+                }
+            )*
+        };
+    }
+
+    // Implements `Reminder` trait for `usize`, `u64` and `u128`.
+    impl_reminder_for_large_unsigned_int!(usize, u64, u128);
+
+    macro_rules! impl_reminder_for_small_signed_int {
+        ($($signed_small_int: tt), *) => {
+            $(
+                impl Reminder for $signed_small_int {
+                    fn reminder(self, modulus: InnerType) -> InnerType {
+                        (self as i32 % modulus as i32 + modulus as i32) as InnerType % modulus
+                    }
+                }
+            )*
+        };
+    }
+
+    // Implements `Reminder` trait for `i8`, `i16` and `i32`.
+    impl_reminder_for_small_signed_int!(i8, i16, i32);
+
+    macro_rules! impl_reminder_for_large_signed_int {
+        ($($signed_large_int: tt), *) => {
+            $(
+                impl Reminder for $signed_large_int {
+                    fn reminder(self, modulus: InnerType) -> InnerType {
+                        (self % modulus as Self + modulus as Self) as InnerType % modulus
+                    }
+                }
+            )*
+        };
+    }
+
+    // Implements `Reminder` trait for `isize`, `i64` and `i128`.
+    impl_reminder_for_large_signed_int!(isize, i64, i128);
+
+    /// Structure for modular arithmetic.
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+    pub struct Modint<const MODULUS: InnerType> {
+        rem: InnerType,
+    }
+
+    impl<const MODULUS: InnerType> std::fmt::Display for Modint<MODULUS> {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "{}", self.rem)
+        }
+    }
+
+    impl<const MODULUS: InnerType> Default for Modint<MODULUS> {
+        /// Returns a `Modint` instance equivalent to `0`.
+        fn default() -> Self {
+            Self::raw(0)
+        }
+    }
+
+    impl<T, const MODULUS: InnerType> From<T> for Modint<MODULUS>
+    where
+        T: Reminder,
+    {
+        fn from(value: T) -> Self {
+            Self::new(value)
+        }
+    }
+
+    impl<const MODULUS: InnerType> Add<Modint<MODULUS>> for Modint<MODULUS> {
+        type Output = Self;
+
+        fn add(self, rhs: Modint<MODULUS>) -> Self::Output {
+            Self::raw((self.rem + rhs.rem) % MODULUS)
+        }
+    }
+
+    impl<const MODULUS: InnerType> Sub<Modint<MODULUS>> for Modint<MODULUS> {
+        type Output = Self;
+
+        fn sub(self, rhs: Modint<MODULUS>) -> Self::Output {
+            Self::raw((self.rem + MODULUS - rhs.rem) % MODULUS)
+        }
+    }
+
+    impl<const MODULUS: InnerType> Mul<Modint<MODULUS>> for Modint<MODULUS> {
+        type Output = Self;
+
+        fn mul(self, rhs: Modint<MODULUS>) -> Self::Output {
+            Self::raw((self.rem as u64 * rhs.rem as u64 % MODULUS as u64) as InnerType)
+        }
+    }
+
+    impl<const MODULUS: InnerType> Div<Modint<MODULUS>> for Modint<MODULUS> {
+        type Output = Self;
+
+        #[allow(clippy::suspicious_arithmetic_impl)]
+        fn div(self, rhs: Modint<MODULUS>) -> Self::Output {
+            self * rhs.inv()
+        }
+    }
+
+    impl<const MODULUS: InnerType> Neg for Modint<MODULUS> {
+        type Output = Self;
+
+        fn neg(self) -> Self::Output {
+            Self::raw((MODULUS - self.rem) % MODULUS)
+        }
+    }
+
+    impl<const MODULUS: InnerType> AddAssign<Modint<MODULUS>> for Modint<MODULUS> {
+        fn add_assign(&mut self, rhs: Modint<MODULUS>) {
+            *self = *self + rhs;
+        }
+    }
+
+    impl<const MODULUS: InnerType> SubAssign<Modint<MODULUS>> for Modint<MODULUS> {
+        fn sub_assign(&mut self, rhs: Modint<MODULUS>) {
+            *self = *self - rhs;
+        }
+    }
+
+    impl<const MODULUS: InnerType> MulAssign<Modint<MODULUS>> for Modint<MODULUS> {
+        fn mul_assign(&mut self, rhs: Modint<MODULUS>) {
+            *self = *self * rhs;
+        }
+    }
+
+    impl<const MODULUS: InnerType> DivAssign<Modint<MODULUS>> for Modint<MODULUS> {
+        fn div_assign(&mut self, rhs: Modint<MODULUS>) {
+            *self = *self / rhs;
+        }
+    }
+
+    impl<const MODULUS: InnerType, T> Add<T> for Modint<MODULUS>
+    where
+        T: Reminder,
+    {
+        type Output = Modint<MODULUS>;
+
+        fn add(self, rhs: T) -> Self::Output {
+            self + Self::new(rhs)
+        }
+    }
+
+    impl<const MODULUS: InnerType, T> Sub<T> for Modint<MODULUS>
+    where
+        T: Reminder,
+    {
+        type Output = Modint<MODULUS>;
+
+        fn sub(self, rhs: T) -> Self::Output {
+            self - Self::new(rhs)
+        }
+    }
+
+    impl<const MODULUS: InnerType, T> Mul<T> for Modint<MODULUS>
+    where
+        T: Reminder,
+    {
+        type Output = Modint<MODULUS>;
+
+        fn mul(self, rhs: T) -> Self::Output {
+            self * Self::new(rhs)
+        }
+    }
+
+    impl<const MODULUS: InnerType, T> Div<T> for Modint<MODULUS>
+    where
+        T: Reminder,
+    {
+        type Output = Modint<MODULUS>;
+
+        fn div(self, rhs: T) -> Self::Output {
+            self / Self::new(rhs)
+        }
+    }
+
+    impl<const MODULUS: InnerType, T> AddAssign<T> for Modint<MODULUS>
+    where
+        T: Reminder,
+    {
+        fn add_assign(&mut self, rhs: T) {
+            *self += Modint::new(rhs);
+        }
+    }
+
+    impl<const MODULUS: InnerType, T> SubAssign<T> for Modint<MODULUS>
+    where
+        T: Reminder,
+    {
+        fn sub_assign(&mut self, rhs: T) {
+            *self -= Modint::new(rhs);
+        }
+    }
+
+    impl<const MODULUS: InnerType, T> MulAssign<T> for Modint<MODULUS>
+    where
+        T: Reminder,
+    {
+        fn mul_assign(&mut self, rhs: T) {
+            *self *= Modint::new(rhs);
+        }
+    }
+
+    impl<const MODULUS: InnerType, T> DivAssign<T> for Modint<MODULUS>
+    where
+        T: Reminder,
+    {
+        fn div_assign(&mut self, rhs: T) {
+            *self /= Modint::new(rhs);
+        }
+    }
+
+    impl<const MODULUS: InnerType> Sum<Modint<MODULUS>> for Modint<MODULUS> {
+        fn sum<I: Iterator<Item = Modint<MODULUS>>>(iter: I) -> Self {
+            iter.fold(Self::new(0), |acc, x| acc + x)
+        }
+    }
+
+    impl<'a, const MODULUS: InnerType> Sum<&'a Modint<MODULUS>> for Modint<MODULUS> {
+        fn sum<I: Iterator<Item = &'a Modint<MODULUS>>>(iter: I) -> Self {
+            iter.fold(Self::new(0), |acc, &x| acc + x)
+        }
+    }
+
+    impl<const MODULUS: InnerType> Product<Modint<MODULUS>> for Modint<MODULUS> {
+        fn product<I: Iterator<Item = Modint<MODULUS>>>(iter: I) -> Self {
+            iter.fold(Self::new(1), |acc, x| acc * x)
+        }
+    }
+
+    impl<'a, const MODULUS: InnerType> Product<&'a Modint<MODULUS>> for Modint<MODULUS> {
+        fn product<I: Iterator<Item = &'a Modint<MODULUS>>>(iter: I) -> Self {
+            iter.fold(Self::new(1), |acc, &x| acc * x)
+        }
+    }
+
+    impl<const MODULUS: InnerType> Modint<MODULUS> {
+        /// Returns the modulus.
+        pub fn modulus() -> InnerType {
+            MODULUS
+        }
+
+        /// Returns a `Modint` instance equivalent to `a`.
+        pub fn new<T>(a: T) -> Self
+        where
+            T: Reminder,
+        {
+            Self {
+                rem: a.reminder(MODULUS),
+            }
+        }
+
+        /// Creates a `Modint` instance from a non-negative integer less than `MODULUS`.
+        pub fn raw(a: InnerType) -> Self {
+            Self { rem: a }
+        }
+
+        /// Returns `x` such that `x * q` is equivalent to `p`.
+        pub fn frac<T>(p: T, q: T) -> Self
+        where
+            T: Reminder,
+        {
+            Self::new(p) / Self::new(q)
+        }
+
+        /// Returns the remainder divided by `MODULUS`.
+        /// The returned value is a non-negative integer less than `MODULUS`.
+        pub fn rem(self) -> InnerType {
+            self.rem
+        }
+
+        /// Returns the modular multiplicative inverse.
+        pub fn inv(self) -> Self {
+            Self {
+                rem: modinv(self.rem, MODULUS),
+            }
+        }
+
+        /// Calculates the power of `exp` using the iterative squaring method.
+        pub fn pow<T>(self, exp: T) -> Self
+        where
+            T: ToExponent,
+        {
+            let mut ret = Self::new(1);
+            let mut mul = self;
+            let exp = exp.to_exponent();
+            let mut t = exp.abs;
+
+            while t != 0 {
+                if t & 1 == 1 {
+                    ret *= mul;
+                }
+
+                mul *= mul;
+                t >>= 1;
+            }
+
+            if exp.neg {
+                ret = ret.inv();
+            }
+
+            ret
+        }
+    }
+
+    pub struct Exponent {
+        neg: bool,
+        abs: u128,
+    }
+
+    pub trait ToExponent {
+        fn to_exponent(self) -> Exponent;
+    }
+
+    macro_rules! impl_to_exponent_for_unsigned_int {
+        ($($ty: tt), *) => {
+            $(
+                impl ToExponent for $ty {
+                    fn to_exponent(self) -> Exponent {
+                        Exponent {
+                            neg: false,
+                            abs: self as u128,
+                        }
+                    }
+                }
+            )*
+        };
+    }
+
+    impl_to_exponent_for_unsigned_int!(usize, u8, u16, u32, u64, u128);
+
+    macro_rules! impl_to_exponent_for_signed_int {
+        ($($ty: tt), *) => {
+            $(
+                impl ToExponent for $ty {
+                    fn to_exponent(self) -> Exponent {
+                        Exponent {
+                            neg: self.is_negative(),
+                            abs: self.abs() as u128,
+                        }
+                    }
+                }
+            )*
+        };
+    }
+
+    impl_to_exponent_for_signed_int!(isize, i8, i16, i32, i64, i128);
+
+    /// The type `Modint` with 1000000007 as the modulus.
+    pub type Modint1000000007 = Modint<1000000007>;
+
+    /// The type `Modint` with 998244353 as the modulus.
+    pub type Modint998244353 = Modint<998244353>;
+}
+
+pub mod factorial {
+    use std::ops::{Div, Mul};
+
+    pub struct Factorial<T> {
+        fac: Vec<T>,
+    }
+
+    impl<T> Default for Factorial<T>
+    where
+        T: Clone + From<usize> + Mul<Output = T> + Div<Output = T>,
+    {
+        fn default() -> Self {
+            Self::new()
+        }
+    }
+
+    impl<T> Factorial<T>
+    where
+        T: Clone + From<usize> + Mul<Output = T> + Div<Output = T>,
+    {
+        /// Constructs a new, empty `Factorial<T>`.
+        pub fn new() -> Self {
+            Self {
+                fac: vec![T::from(1)],
+            }
+        }
+
+        /// Returns the factorial of `n`.
+        pub fn factorial(&mut self, n: usize) -> T {
+            if self.fac.len() < n + 1 {
+                for i in (self.fac.len() - 1)..n {
+                    self.fac.push(self.fac[i].clone() * (i + 1).into());
+                }
+            }
+            self.fac[n].clone()
+        }
+
+        /// Returns the number of choices when selecting `k` from `n` and arranging them in a row.
+        pub fn permutations(&mut self, n: usize, k: usize) -> T {
+            if n < k {
+                T::from(0)
+            } else {
+                self.factorial(n) / self.factorial(n - k)
+            }
+        }
+
+        /// Returns the number of choices to select `k` from `n`.
+        pub fn combinations(&mut self, n: usize, k: usize) -> T {
+            if n < k {
+                T::from(0)
+            } else {
+                self.factorial(n) / (self.factorial(k) * self.factorial(n - k))
+            }
+        }
+
+        /// Calculate the number of cases when sample of `k` elements from a set of `n` elements, allowing for duplicates.
+        pub fn combinations_with_repetition(&mut self, n: usize, k: usize) -> T {
+            if n == 0 {
+                if k == 0 {
+                    T::from(1)
+                } else {
+                    T::from(0)
+                }
+            } else {
+                self.combinations(n + k - 1, k)
+            }
+        }
+    }
+}
