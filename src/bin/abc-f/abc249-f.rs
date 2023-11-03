f1f7c8cb50c234c24c11bec1295a1ea15d41b5d2
atcoder8 <atcoder8@gmail.com>
2023-11-04 01:44:27 +0900

    Solved ABC249-F again.

--- src/bin/abc-f/abc249-f.rs
+++ src/bin/abc-f/abc249-f.rs
@@ -1,4 +1,4 @@
-use std::{cmp::Reverse, collections::BinaryHeap};
+use std::collections::BinaryHeap;
 
 use itertools::enumerate;
 use proconio::input;
@@ -21,40 +21,28 @@ fn main() {
     let mut ans = *xx.last().unwrap();
     let mut sum = 0;
     let mut del_heap: BinaryHeap<i64> = BinaryHeap::new();
-    let mut rem_heap: BinaryHeap<Reverse<i64>> = BinaryHeap::new();
-    let mut del_replace_cnt = 0;
+    let mut assign_cnt = 0;
     for (i, &(t, y)) in enumerate(&ty).rev() {
         if t == 1 {
-            ans = ans.max(y + sum);
-
-            if del_replace_cnt == k {
+            if assign_cnt == k {
                 break;
             }
 
-            if del_heap.len() + del_replace_cnt == k {
-                let add_val = del_heap.pop().unwrap();
-                rem_heap.push(Reverse(add_val));
-                sum += add_val;
+            assign_cnt += 1;
+
+            if del_heap.len() + assign_cnt > k {
+                sum += del_heap.pop().unwrap();
             }
-            del_replace_cnt += 1;
 
             ans = ans.max(xx[i] + sum);
         } else {
             if y >= 0 {
                 sum += y;
-            } else if del_heap.len() + del_replace_cnt < k {
-                del_heap.push(y);
             } else {
-                rem_heap.push(Reverse(y));
-                sum += y;
-
-                let del_val = rem_heap.pop().unwrap().0;
-                del_heap.push(del_val);
-                sum -= del_val;
-
-                let add_val = del_heap.pop().unwrap();
-                rem_heap.push(Reverse(add_val));
-                sum += add_val;
+                del_heap.push(y);
+                if del_heap.len() + assign_cnt > k {
+                    sum += del_heap.pop().unwrap();
+                }
             }
 
             ans = ans.max(xx[i] + sum);
