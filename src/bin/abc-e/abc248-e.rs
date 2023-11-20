3c5bbea473570e722ebec9367dc342b1cc2941e8
atcoder8 <atcoder8@gmail.com>
2023-11-21 01:38:02 +0900

    Solved ABC248-E.

---
+++ src/bin/abc-e/abc248-e.rs
@@ -0,0 +1,59 @@
+use im_rc::HashSet;
+use itertools::Itertools;
+use proconio::input;
+
+fn main() {
+    match solve() {
+        Some(ans) => println!("{}", ans),
+        None => println!("Infinity"),
+    }
+}
+
+fn solve() -> Option<usize> {
+    input! {
+        (n, k): (usize, usize),
+        mut xy: [(i64, i64); n],
+    }
+
+    if k == 1 {
+        return None;
+    }
+
+    let mut ans = 0;
+    let mut used = HashSet::new();
+    for i in 0..n {
+        let (x1, y1) = xy[i];
+
+        for j in i + 1..n {
+            let (x2, y2) = xy[j];
+
+            if used.contains(&(i, j)) {
+                continue;
+            }
+
+            let (diff_x_1, diff_y_1) = (x2 - x1, y2 - y1);
+
+            let mut cnt = 2;
+            let mut points = vec![i, j];
+            for k in j + 1..n {
+                let (x3, y3) = xy[k];
+                let (diff_x_2, diff_y_2) = (x3 - x1, y3 - y1);
+
+                if diff_x_1 * diff_y_2 == diff_x_2 * diff_y_1 {
+                    cnt += 1;
+                    points.push(k);
+                }
+            }
+
+            if cnt >= k {
+                ans += 1;
+            }
+
+            for pair in points.into_iter().combinations(2) {
+                used.insert((pair[0], pair[1]));
+            }
+        }
+    }
+
+    Some(ans)
+}
