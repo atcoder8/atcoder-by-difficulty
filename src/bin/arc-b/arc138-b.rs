a034943dd1e91f52c4548c344ef0ce1d91f39255
atcoder8 <atcoder8@gmail.com>
2023-12-06 13:32:23 +0900

    Solved ARC138-B again.

--- src/bin/arc-b/arc138-b.rs
+++ src/bin/arc-b/arc138-b.rs
@@ -1,13 +1,21 @@
 use itertools::Itertools;
-use proconio::input;
 
 fn main() {
-    input! {
-        n: usize,
-        aa: [usize; n],
-    }
+    let n = {
+        let mut line = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+        line.trim().parse::<usize>().unwrap()
+    };
+    let aa: Vec<_> = {
+        let mut line = String::new();
+        line.reserve(2 * n - 1);
+        std::io::stdin().read_line(&mut line).unwrap();
+        line.split_whitespace()
+            .map(|x| x.bytes().next().unwrap())
+            .collect()
+    };
 
-    let flip_num = (0..n).take_while(|&i| aa[i] == i % 2).count();
+    let flip_num = (0..n).take_while(|&i| aa[i] & 1 == (i & 1) as u8).count();
     let ans = aa[flip_num..].iter().dedup().count() <= flip_num;
 
     println!("{}", if ans { "Yes" } else { "No" });
